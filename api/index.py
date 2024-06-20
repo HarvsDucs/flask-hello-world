b3bb70f52d50bbed8626c1b82383890a3a67877f
HarvsDucs <124319998+HarvsDucs@users.noreply.github.com>
2024-06-20 11:52:38 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({
-        'message': 'Hello World'
-    })
+    return {'message':'hello world'}
 
 @app.route('/about')
 def about():
