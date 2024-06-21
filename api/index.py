98b7960fb8f92714c3526f3007505f639ffd9054
HarvsDucs <124319998+HarvsDucs@users.noreply.github.com>
2024-06-21 09:18:01 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from function import plustwo
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return {'message':'hello world'}
+    x = 2
+    return f"Message is: {plustwo(x)}"
 
 @app.route('/about')
 def about():
